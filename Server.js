951cb87b30ef2326aba53c973b16bd3e1b2f8eae
1921622004 <1921622004qwer@gmail.com>
2019-04-09 22:25:54 +0800

    修改模版文件地址

--- Server.js
+++ Server.js
@@ -7,102 +7,98 @@ const fs = require('mz/fs');
 const ejs = require('ejs');
 const DIR = 'dir';
 
-const template = fs.readFileSync('./template.html', 'utf-8');
+const template = fs.readFileSync(path.join(__dirname,'./template.html'), 'utf-8');
 const favIconPath = 'file:' + path.join(__dirname, 'favicon.ico');
 
 class Server {
-    constructor(option = {}) {
-        this.dir = option.dir;
-        this.port = option.port;
-        this.host = option.host;
-        this.template = template;
-        this.handleRequest = this.handleRequest.bind(this);
+  constructor(option = {}) {
+    this.dir = option.dir;
+    this.port = option.port;
+    this.host = option.host;
+    this.template = template;
+    this.handleRequest = this.handleRequest.bind(this);
+  }
+
+  async handleRequest(req, res) {
+    const { pathname } = url.parse(req.url);
+    const currentPath = path.join(this.dir, pathname);
+    try {
+      const statObj = await fs.stat(currentPath);
+      if (statObj.isDirectory()) {
+        const files = await fs.readdir(currentPath);
+        const promiseAry = files.map(file => {
+          const absPath = path.join(currentPath, file);
+          return new Promise((resolve, reject) => {
+            fs.stat(absPath, (err, stats) => {
+              if (stats.isDirectory()) {
+                resolve({
+                  type: DIR,
+                  name: file,
+                  link: path.join(pathname, file)
+                })
+              } else {
+                resolve({
+                  type: mime.getType(absPath),
+                  name: file,
+                  link: path.join(pathname, file)
+                })
+              }
+            })
+          });
+        });
+        const fileList = await Promise.all(promiseAry);
+        let renderResult = ejs.render(this.template, { fileList, favIconPath });
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html;charset=utf-8');
+        res.end(renderResult);
+      } else {
+        this.sendFile(req, res, currentPath, statObj);
+      }
+    } catch (err) {
+      this.sendError(req, res, currentPath);
     }
+  }
 
-    async handleRequest(req, res) {
-        const { pathname } = url.parse(req.url);
-        const currentPath = path.join(this.dir, pathname);
-        try {
-            const statObj = await fs.stat(currentPath);
-            if (statObj.isDirectory()) {
-                const files = await fs.readdir(currentPath);
-                const promiseAry = files.map(file => {
-                    const absPath = path.join(currentPath, file);
-                    return new Promise((resolve, reject) => {
-                        fs.stat(absPath, (err, stats) => {
-                            if (stats.isDirectory()) {
-                                resolve({
-                                    type: DIR,
-                                    name: file,
-                                    link: path.join(pathname, file)
-                                })
-                            } else {
-                                resolve({
-                                    type: mime.getType(absPath),
-                                    name: file,
-                                    link: path.join(pathname, file)
-                                })
-                            }
-                        })
-                    });
-                });
-                const fileList = await Promise.all(promiseAry);
-                console.log(fileList);
+  sendError(req, res, currentPath) {
+    res.statusCode = 404;
+    res.end(`can not found ${currentPath}`);
+  }
 
-                let renderResult = ejs.render(this.template, { fileList, favIconPath });
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'text/html;charset=utf-8');
-                res.end(renderResult);
-            } else {
-                this.sendFile(req, res, currentPath, statObj);
-            }
-        } catch (err) {
-            console.log(err);
+  isCache(req, res, statObj) {
+    const ifModifiedSince = req.headers['if-modified-since'];
+    const ifNoneMatch = req.headers['if-none-match'];
+    const lastModified = statObj.cTime + '';
+    const eTag = statObj.size + '';
+    const expireTime = new Date(Date.now() + 180 * 1000);
+    if (eTag === ifNoneMatch) {
+      return true
+    };
+    if (lastModified === ifModifiedSince) {
+      return true
+    };
+    res.setHeader('Cache-Control', 'max-age=180');
+    res.setHeader('ETag', eTag);
+    res.setHeader('Last-Modified', lastModified);
+    res.setHeader('Expires', `${expireTime.toUTCString()}`);
+    return false
+  }
 
-            this.sendError(req, res, currentPath);
-        }
+  sendFile(req, res, currentPath, statObj) {
+    if (this.isCache(req, res, statObj)) {
+      res.statusCode = 304;
+      res.end();
+    } else {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', `${mime.getType(currentPath)};charset=utf-8`);
+      fs.createReadStream(currentPath).pipe(res);
     }
+  }
 
-    sendError(req, res, currentPath) {
-        res.statusCode = 404;
-        res.end(`can not found ${currentPath}`);
-    }
-
-    isCache(req, res, statObj) {
-        const ifModifiedSince = req.headers['if-modified-since'];
-        const ifNoneMatch = req.headers['if-none-match'];
-        const lastModified = statObj.cTime + '';
-        const eTag = statObj.size + '';
-        const expireTime = new Date(Date.now() + 180 * 1000);
-        if (eTag === ifNoneMatch) {
-            return true
-        };
-        if (lastModified === ifModifiedSince) {
-            return true
-        };
-        res.setHeader('Cache-Control', 'max-age=180');
-        res.setHeader('ETag', eTag);
-        res.setHeader('Last-Modified', lastModified);
-        res.setHeader('Expires', `${expireTime.toUTCString()}`);
-        return false
-    }
-
-    sendFile(req, res, currentPath, statObj) {
-        if (this.isCache(req, res, statObj)) {
-            res.statusCode = 304;
-            res.end();
-        } else {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', `${mime.getType(currentPath)};charset=utf-8`);
-            fs.createReadStream(currentPath).pipe(res);
-        }
-    }
-
-    start() {
-        http.createServer(this.handleRequest).listen(this.port, () => {
-            console.log(chalk.green(`live server start at ${this.port}`))
-        })
-    }
+  start() {
+    http.createServer(this.handleRequest).listen(this.port, () => {
+      console.log(chalk.green(`live server start at ${this.port}`))
+    })
+  }
 }
 
 module.exports = Server
