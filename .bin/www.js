4be735ecc54e37e0a15febdaf355885a7b74b026
1921622004 <1921622004qwer@gmail.com>
2019-04-11 22:40:45 +0800

    删除host

--- .bin/www.js
+++ .bin/www.js
@@ -5,12 +5,10 @@ const package = require('../package.json');
 
 const option = {
     port: 4444,
-    host: "localhost",
     dir: process.cwd()
 }
 
 program.option('-p --port <n>', '设置端口号 默认4444')
-    .option('-o --host <n>', '')
     .option('-d --dir <n>', '静态服务目录，默认当前目录')
     .version(package.version)
     .parse(process.argv)
--- Server.js
+++ Server.js
@@ -15,7 +15,6 @@ class Server {
   constructor(option = {}) {
     this.dir = option.dir;
     this.port = option.port;
-    this.host = option.host;
     this.template = template;
     this.handleRequest = this.handleRequest.bind(this);
   }
